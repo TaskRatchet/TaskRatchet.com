0702fb51a3341ab8cff36a006461fd4baa3edd32
Nathan Arthur <nathan@nathanarthur.com>
2024-11-29 14:43:56 -0500

    add astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
