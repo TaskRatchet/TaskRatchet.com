5ab87e8f9b669049d425fbb617ca051837f41e27
Nathan Arthur <nathan@nathanarthur.com>
2025-01-15 12:35:31 -0500

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -12,22 +12,31 @@
       <div class="footer-section">
         <h4>Legal</h4>
         <ul>
-          <li><a href="/terms">Terms of Service</a></li>
-          <li><a href="/privacy">Privacy Policy</a></li>
+          <li>
+            <a href="https://docs.taskratchet.com/terms.html"
+              >Terms of Service</a
+            >
+          </li>
+          <li>
+            <a href="https://docs.taskratchet.com/privacy.html"
+              >Privacy Policy</a
+            >
+          </li>
         </ul>
       </div>
       <div class="footer-section">
         <h4>Connect</h4>
         <ul>
-          <li><a href="https://twitter.com/taskratchet">Twitter</a></li>
-          <li><a href="https://github.com/narthur/taskratchet">GitHub</a></li>
+          <li><a href="https://github.com/taskratchet">GitHub</a></li>
           <li><a href="mailto:support@taskratchet.com">Contact Us</a></li>
         </ul>
       </div>
     </div>
     <div class="footer-bottom">
       <p>Built with ❤️ by Narthur and Pine Peak Digital.</p>
-      <p class="copyright">&copy; {new Date().getFullYear()} TaskRatchet. All rights reserved.</p>
+      <p class="copyright">
+        &copy; {new Date().getFullYear()} TaskRatchet. All rights reserved.
+      </p>
     </div>
   </div>
 </footer>
